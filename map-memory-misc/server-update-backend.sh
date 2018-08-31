69dfba095e948602a42acdd17fe8ad4d1f66c507
fengchang <fengchang_fight@163.com>
2018-08-31 10:04:40 +0800

    update

--- map-memory-misc/server-update-backend.sh
+++ map-memory-misc/server-update-backend.sh
@@ -4,8 +4,13 @@ cd /root/go/src/map-memory-backend/
 git pull
 go install
 
-export PID=`netstat -tulpn  | grep 8042 | awk '{print $NF}' | sed 's/\/\.\/map-memory-*//'`
+#export PID=`netstat -tulpn  | grep 8042 | awk '{print $NF}' | sed 's/\/\.\/map-memory-*//'`
+export PID=`netstat -tulpn  | grep 8042 | awk '{print $NF}' | tr -d -c 0-9`
+
+echo "Killing process $PID ..."
 kill -9 $PID
 
+echo "Switching to go bin directory..."
 cd /root/go/bin
+echo "Restarting the newly built process..."
 nohup ./map-memory-backend prod &
